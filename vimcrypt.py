0c5ec729366920d7e4d746f03a4a1374d636422b
Charlie Collard <charliecollard0@gmail.com>
2017-03-14 16:11:58 +0000

    Removed useless '0b' string

--- vimcrypt.py
+++ vimcrypt.py
@@ -20,7 +20,7 @@ out_bytes = bytearray()
 for char in text_input:
     # Give each ascii byte some room to breathe by expanding it to 2-5 bytes randomly
     byte_count = randint(2,6)
-    first = int("0b" + "1"*byte_count + "0"*(8-byte_count), 2)
+    first = int("1"*byte_count + "0"*(8-byte_count), 2)
     out_bytes.append(first)
 
     # Append n-1 continuation bytes
